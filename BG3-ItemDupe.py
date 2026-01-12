0f09007c91682a4531c4318a49a5f2c9498f3706
rautatie <eino.ahvenainen79@gmail.com>
2026-01-12 18:09:03 +0200

    Fix formatting

--- BG3-ItemDupe.py
+++ BG3-ItemDupe.py
@@ -80,7 +80,7 @@ def main():
                 if(int(choice[1]) < 3):
                     dupe((left+57*(int(choice[1])-1),bottom))
                 elif(int(choice[1]) < 5):
-                    dupe((right-57*((int(choice[1]))%2),bottom))
+                    dupe((right-57*((int(choice[1])-2)%2),bottom))
                 else:
                     dupe((left+(right-left)*(int(choice[1])-5),bottom-(int(57*1.5))))
             case 3:
@@ -91,3 +91,4 @@ def main():
 if __name__ == "__main__":
 
     main()
+
